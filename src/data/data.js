06232e32439144607f008530b3c583c489ed4e0b
Barrett Hiltabrand <bear2927@yahoo.com>
2023-06-07 22:24:38 -0500

    Finished Portfolio Website

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import HousingPlus from '../assets/projects/HousingPlus.png'
+import Yellowstone from '../assets/projects/YellowstoneNationalPark.png'
+import ASL from '../assets/projects/ASL.png'
+import VideoGames from '../assets/projects/VideoGames.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:HousingPlus,
+        github:"https://github.com/Bear2927/Housing-Plus-Project",
+        live:"https://www.loom.com/share/c964834414e54263ba19ca8324ce74f3",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Yellowstone,
+        github:"https://github.com/Bear2927/Yellowstone-Naitonal-Park-Project",
+        live:"https://www.loom.com/share/058fdca580424e82aef88ff376f9e5b4",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:VideoGames,
+        github:"https://github.com/Bear2927/Phase3Project_VideoGames",
+        live:"https://www.loom.com/share/1112157449dc4c5c8ae939fb8c945210",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:ASL,
+        github:"https://github.com/Bear2927/asl-project",
+        live:"",
+    },
+
+
+]
